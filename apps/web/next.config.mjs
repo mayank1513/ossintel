18e5445e9d0de4398be94e5f6201c227b354e2de
Mayank <mayank.srmu@gmail.com>
2026-07-20 00:07:37 +0530

    chore: :wrench: add rewrites

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -9,6 +9,17 @@ const config = {
   images: {
     unoptimized: true,
   },
+  rewrites: async () => [
+    {
+      source: "/namaste-demo",
+      destination: "https://youtu.be/AVXvVPlzLEA",
+    },
+    {
+      source: "/namaste-pitchdeck",
+      destination:
+        "https://docs.google.com/presentation/d/1ylToostn_PzvF9x4wIJeqpR92oK2ROyb/edit?usp=sharing&ouid=106538074401014575642&rtpof=true&sd=true",
+    },
+  ],
 };
 
 export default withMDX(config);
